b2ee30466c0d51a5e54d35c4b2efb7a104a10b80
Ogan Belema <Oganbelema@gmail.com>
2017-08-17 17:37:24 +0100

    Added app logo and also palette to change appbar and fab color

--- app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/Main2Activity.java
+++ app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/Main2Activity.java
@@ -1,25 +1,26 @@
 package com.example.ogan.listofdevelopersinlagosgithub;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.Window;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.ogan.listofdevelopersinlagosgithub.APIgson.ApiResult;
-import com.example.ogan.listofdevelopersinlagosgithub.APIgson.GetData;
-import com.example.ogan.listofdevelopersinlagosgithub.APIgson.Item;
 import com.example.ogan.listofdevelopersinlagosgithub.APIgson.UserGson.GetUser;
 import com.example.ogan.listofdevelopersinlagosgithub.APIgson.UserGson.UserApi;
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
+import com.squareup.picasso.Target;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
@@ -38,7 +39,8 @@ public class Main2Activity extends AppCompatActivity {
     GetUser getUser;
     private ProgressBar progressBar;
     CardView cardView;
-    FrameLayout frameLayout;
+    CoordinatorLayout coordinatorLayout;
+    Toolbar toolbar;
 
     private static final String URL = "https://api.github.com/";
 
@@ -54,15 +56,13 @@ public class Main2Activity extends AppCompatActivity {
         txtFullName = (TextView) findViewById(R.id.user_name);
         repo = (TextView) findViewById(R.id.repo_number);
         cardView = (CardView) findViewById(R.id.course_card);
-        frameLayout = (FrameLayout) findViewById(R.id.rootLayout);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl(URL).
                 addConverterFactory(GsonConverterFactory.create()).build();
 
         getUser = retrofit.create(GetUser.class);
-
-
-
 
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
@@ -70,7 +70,11 @@ public class Main2Activity extends AppCompatActivity {
         final String userUrl = intent.getStringExtra("url");
         String avatar = intent.getStringExtra("avatar");
 
-        setTitle(uppercaseUsername);
+        //setTitle(uppercaseUsername);
+        toolbar.setTitle(uppercaseUsername);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
 
 
@@ -81,17 +85,23 @@ public class Main2Activity extends AppCompatActivity {
                 System.out.println("error" + response.errorBody());
                 System.out.println("code" + response.code());
 
-                progressBar.setVisibility(View.INVISIBLE);
+
                 UserApi userResult = response.body();
 
                 if (userResult != null) {
                     String fullName = userResult.getName();
-                    txtFullName.setText(fullName);
+                    if(fullName != null){
+                        txtFullName.setText(fullName);
+                    }
                     String repoNumber = userResult.getPublicRepos().toString();
-                    repo.setText(repoNumber);
+                    repo.setText(repoNumber + " repositories");
+                    progressBar.setVisibility(View.INVISIBLE);
                     cardView.setVisibility(View.VISIBLE);
 
                 } else {
+
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Error loading data", Toast.LENGTH_SHORT).show();
 
                 }
 
@@ -102,13 +112,6 @@ public class Main2Activity extends AppCompatActivity {
 
                 progressBar.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(), "An error occurred while trying to get data. Please check network connection and try again.", Toast.LENGTH_SHORT).show();
-                /* Snackbar.make(frameLayout, "An error occurred while trying to get data.", Snackbar.LENGTH_LONG).setAction("Retry", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent1 = new Intent(getApplicationContext(), Main2Activity.class);
-                        startActivity(intent1);
-                    }
-                }).show(); */
 
                 System.out.println("t" + t.getMessage());
 
@@ -131,7 +134,37 @@ public class Main2Activity extends AppCompatActivity {
 
         Picasso.with(getApplicationContext()).load(avatar).into(imgAvatar);
 
+        Picasso.with(getApplicationContext()).load(avatar)
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        Palette.from(bitmap)
+                                .generate(new Palette.PaletteAsyncListener() {
+                                    @Override
+                                    public void onGenerated(Palette palette) {
+                                        Palette.Swatch swatch = palette.getVibrantSwatch();
+                                        if (swatch == null) {
+                                            return;
+                                        }
 
+                                        button.setBackgroundTintList(ColorStateList.valueOf(swatch.getRgb()));
+                                        toolbar.setBackgroundColor(swatch.getRgb());
+                                        toolbar.setTitleTextColor(swatch.getTitleTextColor());
+
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                    }
+                });
 
     }
 
