f0f0b170f8a4d1db8fe7ed65bbd3da2f52fd3df9
Ogan Belema <Oganbelema@gmail.com>
2017-08-30 16:05:15 +0100

    changed the colors to github brand colors

---
+++ app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/APIgson/ApiResult.java
@@ -0,0 +1,45 @@
+
+package com.example.ogan.listofdevelopersinlagosgithub.APIgson;
+
+import java.util.ArrayList;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ApiResult {
+
+    @SerializedName("total_count")
+    @Expose
+    private Integer totalCount;
+    @SerializedName("incomplete_results")
+    @Expose
+    private Boolean incompleteResults;
+    @SerializedName("items")
+    @Expose
+    private ArrayList<Item> items = null;
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Boolean getIncompleteResults() {
+        return incompleteResults;
+    }
+
+    public void setIncompleteResults(Boolean incompleteResults) {
+        this.incompleteResults = incompleteResults;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+}
