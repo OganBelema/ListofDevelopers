7f5e84d5366e928b1eb199b19310a1aa7c7ef09e
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-07-30 10:24:24 +0100

    Fixed bugs in landscape layout Changed application id Replaced Service with IntentService Updated google-services.json

--- app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/database/InsertResultService.java
+++ app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/database/InsertResultService.java
@@ -1,50 +1,38 @@
 package com.example.ogan.listofdevelopersinlagosgithub.database;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+
+import androidx.annotation.Nullable;
 
 import com.example.ogan.listofdevelopersinlagosgithub.common.CustomApplication;
 import com.example.ogan.listofdevelopersinlagosgithub.model.items.ApiResult;
 
-import io.reactivex.Completable;
-import io.reactivex.functions.Action;
-import io.reactivex.schedulers.Schedulers;
-
-public class InsertResultService extends Service {
+public class InsertResultService extends IntentService {
 
     public static final String RESULT_KEY = "result";
 
     DeveloperDatabase mDeveloperDatabase;
 
+    public InsertResultService() {
+        super("InsertResultService");
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-
-        mDeveloperDatabase = ((CustomApplication) getApplication()).getApplicationComponent().getDeveloperDatabase();
+        mDeveloperDatabase = ((CustomApplication) this.getApplication()).getApplicationComponent().getDeveloperDatabase();
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        ApiResult result = intent.getParcelableExtra(RESULT_KEY);
-
-        cacheData(result);
-
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null){
+            ApiResult result = intent.getParcelableExtra(RESULT_KEY);
+            cacheData(result);
+        }
     }
 
     private void cacheData(final ApiResult apiResult) {
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-                mDeveloperDatabase.getResultDao().insertResult(apiResult);
-            }
-        }).subscribeOn(Schedulers.io())
-                .subscribe();
+        mDeveloperDatabase.getResultDao().insertResult(apiResult);
     }
 }
--- app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/database/InsertUserService.java
+++ app/src/main/java/com/example/ogan/listofdevelopersinlagosgithub/database/InsertUserService.java
@@ -1,50 +1,38 @@
 package com.example.ogan.listofdevelopersinlagosgithub.database;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+
+import androidx.annotation.Nullable;
 
 import com.example.ogan.listofdevelopersinlagosgithub.common.CustomApplication;
 import com.example.ogan.listofdevelopersinlagosgithub.model.users.UserApi;
 
-import io.reactivex.Completable;
-import io.reactivex.functions.Action;
-import io.reactivex.schedulers.Schedulers;
-
-public class InsertUserService extends Service {
+public class InsertUserService extends IntentService {
 
     public static final String USER_KEY = "user";
 
     DeveloperDatabase mDeveloperDatabase;
 
+    public InsertUserService() {
+        super("InsertUserService");
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-
         mDeveloperDatabase = ((CustomApplication) getApplication()).getApplicationComponent().getDeveloperDatabase();
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        UserApi userApi = intent.getParcelableExtra(USER_KEY);
-
-        cacheData(userApi);
-
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null){
+            UserApi userApi = intent.getParcelableExtra(USER_KEY);
+            cacheData(userApi);
+        }
     }
 
     private void cacheData(final UserApi userApi) {
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-               mDeveloperDatabase.getUserDao().insertUser(userApi);
-            }
-        }).subscribeOn(Schedulers.io())
-                .subscribe();
+        mDeveloperDatabase.getUserDao().insertUser(userApi);
     }
 }
